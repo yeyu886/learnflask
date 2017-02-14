1f1d5b0ebe8a68f556a73909817f027d2c1d91f0
yeyu886 <xueyue_xls@qq.com>
2017-02-14 16:42:24 +0800

    123

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,7 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
+
 class LoginForm(Form):
     openId = StringField("openId",validators=[DataRequired()])
     remember_me = BooleanField("remember_me",default=False)
