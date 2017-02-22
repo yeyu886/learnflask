d12789718af416b891235a9d7a124a56bc0b7810
yeyu886 <xueyue_xls@qq.com>
2017-02-22 16:18:39 +0800

    22

--- app/models.py
+++ app/models.py
@@ -11,3 +11,13 @@ class User(db.Model):
     
     def __repr__(self):
         return "<User %r>" % (self.nickname)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer,db.ForeignKey('user_id'))
+
+    def __repr__(self):
+        return "<Post %r>" % (self.body)
